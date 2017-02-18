b95b00230e8df8f7a7ee294ab0904577abeb122f
Theo lampert <theolampert@gmail.com>
2017-02-18 15:59:30 +0100

    Add hashing

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,27 @@
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
+import hash from 'rollup-plugin-hash';
 const env = process.env.NODE_ENV || 'development';
 
+const entry = 'src/app.js';
+const dest = 'dist/bundle.js';
+
 const plugins = [
-  buble()
+  buble(),
 ];
 
 if (env === 'production') {
   const minifier = uglify({}, minify);
-  plugins.push(minifier);
+  const hasher = hash({
+    dest: 'dist/bundle.[hash].js'
+  });
+  plugins.push(minifier, hasher);
 }
 
 export default {
-  entry: 'src/app.js',
-  dest: 'dist/bundle.js',
+  entry,
+  dest,
+  plugins,
   format: 'es',
-  plugins: plugins,
 }
